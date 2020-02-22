60adcce28d61c51c628c1dbc12cd45cd0349cd7d
disverso <60159847+disverso@users.noreply.github.com>
2020-02-23 03:19:41 +0600

    Power of Thor
    Power of Thor solution (C#).

---
+++ power_of_thor.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int lightX = int.Parse(inputs[0]); // the X position of the light of power
+        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
+        int initialTx = int.Parse(inputs[2]); // Thor's starting X position
+        int initialTy = int.Parse(inputs[3]); // Thor's starting Y position
+
+        // game loop
+        while (true)
+        {
+            int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            if (lightX == initialTx && lightY < initialTy) {
+                initialTy--;
+                Console.WriteLine("N");
+            } else if (lightX > initialTx && lightY < initialTy) {
+                initialTx++;
+                initialTy--;
+                Console.WriteLine("NE");
+            } else if (lightX > initialTx && lightY == initialTy) {
+                initialTx++;
+                Console.WriteLine("E");
+            } else if (lightX > initialTx && lightY > initialTy) {
+                initialTx++;
+                initialTy++;
+                Console.WriteLine("SE");
+            } else if (lightX == initialTx && lightY > initialTy) {
+                initialTy++;
+                Console.WriteLine("S");
+            } else if (lightX < initialTx && lightY > initialTy) {
+                initialTx--;
+                initialTy++;
+                Console.WriteLine("SW");
+            } else if (lightX < initialTx && lightY == initialTy) {
+                initialTx--;
+                Console.WriteLine("W");
+            } else if (lightX < initialTx && lightY < initialTy) {
+                initialTx--;
+                initialTy--;
+                Console.WriteLine("NW");
+            }
+
+            // A single line providing the move to be made: N NE E SE S SW W or NW
+        }
+    }
+}

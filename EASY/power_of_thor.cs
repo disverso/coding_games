60178741a388490cd7064a6f873616836e984bb8
disverso <60159847+disverso@users.noreply.github.com>
2020-03-02 01:31:29 +0600

    Power of Thor - Episode 1
    Power of Thor - Episode 1 Solution
    Link: https://www.codingame.com/ide/puzzle/power-of-thor-episode-1

--- EASY/power_of_thor.cs
+++ EASY/power_of_thor.cs
@@ -1,64 +1,49 @@
 using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- * ---
- * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
- **/
+///
+/// Power of Thor - Episode 1 Solution
+/// Link: https://www.codingame.com/ide/puzzle/power-of-thor-episode-1
+///
+
 class Player
 {
-    static void Main(string[] args)
-    {
-        string[] inputs = Console.ReadLine().Split(' ');
-        int lightX = int.Parse(inputs[0]); // the X position of the light of power
-        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
-        int initialTx = int.Parse(inputs[2]); // Thor's starting X position
-        int initialTy = int.Parse(inputs[3]); // Thor's starting Y position
+	static void Main(string[] args)
+	{
+		string[] inputs = Console.ReadLine().Split(' ');
+		int lightX = int.Parse(inputs[0]); // the X position of the light of power
+		int lightY = int.Parse(inputs[1]); // the Y position of the light of power
+		int initialTx = int.Parse(inputs[2]); // Thor's starting X position
+		int initialTy = int.Parse(inputs[3]); // Thor's starting Y position
+		string output = "";
+		
+		// game loop
+		while (true)
+		{
+			int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
+			if (lightY < initialTy)
+			{
+				initialTy--;
+				output += "N";
+			}
+			else if (lightY > initialTy)
+			{
+				initialTy++;
+				output += "S";
+			}
 
-        // game loop
-        while (true)
-        {
-            int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
+			if (lightX > initialTx)
+			{
+				initialTx++;
+				output += "E";
+			}
+			else if (lightX < initialTx)
+			{
+				initialTx--;
+				output += "W";
+			}
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-            if (lightX == initialTx && lightY < initialTy) {
-                initialTy--;
-                Console.WriteLine("N");
-            } else if (lightX > initialTx && lightY < initialTy) {
-                initialTx++;
-                initialTy--;
-                Console.WriteLine("NE");
-            } else if (lightX > initialTx && lightY == initialTy) {
-                initialTx++;
-                Console.WriteLine("E");
-            } else if (lightX > initialTx && lightY > initialTy) {
-                initialTx++;
-                initialTy++;
-                Console.WriteLine("SE");
-            } else if (lightX == initialTx && lightY > initialTy) {
-                initialTy++;
-                Console.WriteLine("S");
-            } else if (lightX < initialTx && lightY > initialTy) {
-                initialTx--;
-                initialTy++;
-                Console.WriteLine("SW");
-            } else if (lightX < initialTx && lightY == initialTy) {
-                initialTx--;
-                Console.WriteLine("W");
-            } else if (lightX < initialTx && lightY < initialTy) {
-                initialTx--;
-                initialTy--;
-                Console.WriteLine("NW");
-            }
-
-            // A single line providing the move to be made: N NE E SE S SW W or NW
-        }
-    }
+			Console.WriteLine(output);
+			output = "";
+		}
+	}
 }
